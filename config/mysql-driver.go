bd5f19dd678a72bde2559b5e8171d658fb5680d6
“Nawi” <nawihusen@gmail.com>
2022-08-23 13:00:24 +0800

    kerangka

---
+++ config/mysql-driver.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectToDB() *sql.DB {
+	dbConnection := os.Getenv("DB_CONNECTION")
+	db, err := sql.Open("mysql", dbConnection)
+
+	if err != nil {
+		log.Fatal("error sql Open ", err.Error())
+	}
+
+	errPing := db.Ping()
+	if errPing != nil {
+		log.Fatal("error connect to db ", errPing.Error())
+	} else {
+		fmt.Println("success connect to DB")
+	}
+
+	return db
+
+}
